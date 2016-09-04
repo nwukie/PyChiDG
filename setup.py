6a0c84a897aa509cf08fabbc629c9b6b4c5def73
Nathan A. Wukie <nwukie@gmail.com>
2016-09-04 16:16:56 -0400

    Added pychidg_view

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
       license='BSD 3-Clause',
       keywords='finite element DG galerkin chimera',
       packages=['pychidg'],
-      scripts=['scripts/chidggen'],
+      scripts=['scripts/pychidg_gen','scripts/pychidg_view'],
 #      install_requires=['f90wrap>=0.0.0'],
       ext_modules=[f90wrap_interface],
       )
